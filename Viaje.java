38a5e299e4da347ece0eadfe47f82a541740419b
Cacutss <cuentasmurfdeleague@gmail.com>
2026-08-25 16:13:33 -0300

    Terminado v1 funcionando

--- Viaje.java
+++ Viaje.java
@@ -5,8 +5,8 @@ public class Viaje{
 	final double PREC_GASOIL_LT = 1250.0;
 	final double CAP_TANQUE = 400.0;
 	int kilInicial,kilFinal;
-	double litrosGasoil;
 	int kilRecorridos;
+	double litrosGasoil;
 	double consumoLt;
 	double rendimiento;
 	double costoCombustible;
@@ -22,6 +22,13 @@ public class Viaje{
 	kilFinal = scanner.nextInt();
 	System.out.println("Ingrese los litros de gasoil cargados: ");
 	litrosGasoil = scanner.nextDouble();
-		
+	kilRecorridos = kilFinal - kilInicial;
+	consumoLt = (litrosGasoil / kilRecorridos) * 100;
+	rendimiento = (kilRecorridos / litrosGasoil);
+	costoCombustible = litrosGasoil * PREC_GASOIL_LT;
+	costoKilometro = costoCombustible / kilRecorridos;
+	capacidadPorcentaje = (litrosGasoil / CAP_TANQUE) * 100;
+	System.out.printf("Km recorridos: %d | Consumo cada 100 km: %.2f | Km por litro: %.2f | Costo total: %.2f\n",kilRecorridos,consumoLt,rendimiento,costoCombustible);
+	System.out.printf("La carga representa %.2f%% de la capacidad del tanque\n",capacidadPorcentaje);
     }
 }
