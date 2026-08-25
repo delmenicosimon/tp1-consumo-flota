ecc95269170b70b43f6bf64238b253eb9820ae1e
Cacutss <cuentasmurfdeleague@gmail.com>
2026-08-25 19:05:15 -0300

    C: Prints mas lindos, finalizado

--- Viaje.java
+++ Viaje.java
@@ -1,34 +1,40 @@
 import java.util.Scanner;
 
-public class Viaje{
-    public static void main(String args[]){
-	final double PREC_GASOIL_LT = 1250.0;
-	final double CAP_TANQUE = 400.0;
-	int kilInicial,kilFinal;
-	int kilRecorridos;
-	double litrosGasoil;
-	double consumoLt;
-	double rendimiento;
-	double costoCombustible;
-	double costoKilometro;
-	double capacidadPorcentaje;
-	String patente;
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("Ingrese la patente del camion: ");
-	patente = scanner.nextLine();
-	System.out.println("Ingrese el kilometraje antes del viaje: ");
-	kilInicial = scanner.nextInt();
-	System.out.println("Ingrese el kilometraje despues del viaje: ");
-	kilFinal = scanner.nextInt();
-	System.out.println("Ingrese los litros de gasoil cargados: ");
-	litrosGasoil = scanner.nextDouble();
-	kilRecorridos = kilFinal - kilInicial;
-	consumoLt = (litrosGasoil / kilRecorridos) * 100;
-	rendimiento = (kilRecorridos / litrosGasoil);
-	costoCombustible = litrosGasoil * PREC_GASOIL_LT;
-	costoKilometro = costoCombustible / kilRecorridos;
-	capacidadPorcentaje = (litrosGasoil / CAP_TANQUE) * 100;
-	System.out.printf("Km recorridos: %d | Consumo cada 100 km: %.2f | Km por litro: %.2f | Costo total: %.2f\n",kilRecorridos,consumoLt,rendimiento,costoCombustible);
-	System.out.printf("La carga representa %.2f%% de la capacidad del tanque\n",capacidadPorcentaje);
-    }
+public class Viaje {
+	public static void main(String args[]) {
+		final double PREC_GASOIL_LT = 1250.0;
+		final double CAP_TANQUE = 400.0;
+		int kilInicial, kilFinal;
+		int kilRecorridos;
+		double litrosGasoil;
+		double consumoLt;
+		double rendimiento;
+		double costoCombustible;
+		double costoKilometro;
+		double capacidadPorcentaje;
+		String patente;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese la patente del camion: ");
+		patente = scanner.nextLine();
+		System.out.println("Ingrese el kilometraje antes del viaje: ");
+		kilInicial = scanner.nextInt();
+		System.out.println("Ingrese el kilometraje despues del viaje: ");
+		kilFinal = scanner.nextInt();
+		System.out.println("Ingrese los litros de gasoil cargados: ");
+		litrosGasoil = scanner.nextDouble();
+		kilRecorridos = kilFinal - kilInicial;
+		consumoLt = (litrosGasoil / kilRecorridos) * 100;
+		rendimiento = (kilRecorridos / litrosGasoil);
+		costoCombustible = litrosGasoil * PREC_GASOIL_LT;
+		costoKilometro = costoCombustible / kilRecorridos;
+		capacidadPorcentaje = (litrosGasoil / CAP_TANQUE) * 100;
+		System.out.printf("Camion con patente: %s\n", patente);
+		System.out.println("------------------------------------------");
+		System.out.printf("|Km recorridos   :     %dkm                \n", kilRecorridos);
+		System.out.printf("|Consumo         :     %.2fL/100km         \n", consumoLt);
+		System.out.printf("|Km por litro    :     %.2f/L              \n", rendimiento);
+		System.out.printf("|Costo total     :    $%.2f                \n", costoCombustible);
+		System.out.printf("|Porcentaje carga:     %.2f%%              \n", capacidadPorcentaje);
+		System.out.println("------------------------------------------");
+	}
 }
